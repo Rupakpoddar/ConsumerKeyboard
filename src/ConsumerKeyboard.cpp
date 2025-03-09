4bf6af76b6fb247ae10188fb53031b6e30b2bd57
Rupak Poddar <34826905+Rupakpoddar@users.noreply.github.com>
2025-03-09 16:41:00 -0400

    Update ConsumerKeyboard.cpp

--- src/ConsumerKeyboard.cpp
+++ src/ConsumerKeyboard.cpp
@@ -1,21 +1,19 @@
-// ConsumerKeyboard.cpp
 #include "ConsumerKeyboard.h"
 
 #if defined(_USING_HID)
 
 static const uint8_t _hidConsumerReportDescriptor[] PROGMEM = {
-    // Consumer Control only
-    0x05, 0x0C,                    // USAGE_PAGE (Consumer Devices)
-    0x09, 0x01,                    // USAGE (Consumer Control)
-    0xA1, 0x01,                    // COLLECTION (Application)
-    0x85, 0x03,                    // REPORT_ID (3)
-    0x15, 0x00,                    // LOGICAL_MINIMUM (0)
-    0x26, 0xFF, 0x03,             // LOGICAL_MAXIMUM (1023)
-    0x19, 0x00,                    // USAGE_MINIMUM (0)
-    0x2A, 0xFF, 0x03,             // USAGE_MAXIMUM (1023)
-    0x75, 0x10,                    // REPORT_SIZE (16)
-    0x95, 0x01,                    // REPORT_COUNT (1)
-    0x81, 0x00,                    // INPUT (Data,Ary,Abs)
+    0x05, 0x0C,                    // USAGE_PAGE
+    0x09, 0x01,                    // USAGE
+    0xA1, 0x01,                    // COLLECTION
+    0x85, 0x03,                    // REPORT_ID
+    0x15, 0x00,                    // LOGICAL_MINIMUM
+    0x26, 0xFF, 0x03,              // LOGICAL_MAXIMUM
+    0x19, 0x00,                    // USAGE_MINIMUM
+    0x2A, 0xFF, 0x03,              // USAGE_MAXIMUM
+    0x75, 0x10,                    // REPORT_SIZE
+    0x95, 0x01,                    // REPORT_COUNT
+    0x81, 0x00,                    // INPUT
     0xc0                           // END_COLLECTION
 };
 
@@ -23,10 +21,6 @@ ConsumerKeyboard_::ConsumerKeyboard_(void)
 {
     static HIDSubDescriptor node(_hidConsumerReportDescriptor, sizeof(_hidConsumerReportDescriptor));
     HID().AppendDescriptor(&node);
-}
-
-void ConsumerKeyboard_::begin(void)
-{
 }
 
 void ConsumerKeyboard_::sendReport(uint16_t k)
